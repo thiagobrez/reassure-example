122c852ce1ee053ec9cc0cb73b3f42fb196cdcff
Thiago Brezinski <thiago.brezinski@callstack.com>
2023-03-30 16:57:14 +0100

    chore: wip

--- reassure-tests.sh
+++ reassure-tests.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-echo "===== WORKING DIR: $PWD"
-
 BASELINE_BRANCH=${BASELINE_BRANCH:="main"}
 
 # Required for `git switch` on CI
@@ -10,10 +8,10 @@ git fetch origin
 
 # Gather baseline perf measurements
 git switch "$BASELINE_BRANCH"
-npm install --force
-npm run perf-test --baseline
+yarn install --force
+yarn reassure --baseline
 
 # Gather current perf measurements & compare results
 git switch --detach -
-npm install --force
-npm run perf-test
+yarn install --force
+yarn reassure
