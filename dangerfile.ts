565004956acd951d3fdfc7383e3b290da30fbc61
Thiago Brezinski <thiago.brezinski@callstack.com>
2023-03-31 10:24:37 +0100

    ci: fix reassure config (#4)
    ci: fix reassure config

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,8 +1,5 @@
-/* eslint-disable import/no-extraneous-dependencies */
-// import path from 'path';
 import {dangerReassure} from 'reassure';
 
 dangerReassure({
   inputFilePath: '.reassure/output.md',
-  // inputFilePath: path.join(__dirname, '.reassure/output.md'), //
 });
