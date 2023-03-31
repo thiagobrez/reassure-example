744c71631533931965eb61e4cec50dd158429fad
Thiago Brezinski <thiago.brezinski@callstack.com>
2023-03-31 08:48:49 +0100

    ci(danger): change inputFilePath (#3)
    * ci(danger): change inputFilePath

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,7 +1,8 @@
-// @ts-nocheck
-import path from 'path';
+/* eslint-disable import/no-extraneous-dependencies */
+// import path from 'path';
 import {dangerReassure} from 'reassure';
 
 dangerReassure({
-  inputFilePath: path.join(__dirname, '.reassure/output.md'),
+  inputFilePath: '.reassure/output.md',
+  // inputFilePath: path.join(__dirname, '.reassure/output.md'),
 });
